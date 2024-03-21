4f975f0e1fea993d7c2f422cc34b7aaa520b1134
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-03-22 00:12:33 +0530

    Implementing crud operation on todo API

--- backend/src/model/todoModel.js
+++ backend/src/model/todoModel.js
@@ -4,6 +4,8 @@ const todoSchema = new mongoose.Schema({
   task: {
     type: String,
     required: true,
+    minLength: [5, "Min length should be 5 characters"],
+    maxLength: [100, "Max length should be 100 characters"],
   },
   priority: {
     type: String,
--- backend/src/routers/authRouter.js
+++ backend/src/routers/authRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userModel = require("../model/userModel");
+const todoModel = require("../model/todoModel");
 const bcrypt = require("bcrypt");
 const { isRegexEmail } = require("../utils/authUtils");
 const { isAuth } = require("../middlewares/authMiddleware");
@@ -70,8 +71,29 @@ authRouter
       return res.status(500).send(error.message);
     }
   })
-  .get("/dashboard", isAuth, async (req, res) => {
-    // return res.send("hiii");
+  .get("/all-todos", isAuth, async (req, res) => {
+    try {
+      const username = req.session.user.username;
+      const allTodos = await todoModel.find({ username: username });
+
+      if (allTodos.length === 0) {
+        return res.status(400).send({
+          message: "No todo found",
+        });
+      }
+      console.log(allTodos);
+      res.status(200).send({
+        message: `Success : No of todos found ${allTodos.length}`,
+        data: allTodos,
+      });
+    } catch (error) {
+      return res.status(500).send({
+        message: "Internal server error",
+        error: error.message,
+      });
+    }
+  })
+  .post("/create-todo", isAuth, async (req, res) => {
     try {
       const { task, priority } = req.body;
       const username = req.session.user.username;
@@ -86,10 +108,59 @@ authRouter
         message: "task created successfully.",
       });
     } catch (error) {
-      return res.status(500).send({
-        message: "internal server error",
+      if (error.name === "ValidationError") res.status(400);
+      else res.status(500);
+      res.send({
+        message: error.message,
+      });
+    }
+  })
+  .post("/edit-todo", isAuth, async (req, res) => {
+    try {
+      const { todoId, updatedTask, newPriority } = req.body;
+      const username = req.session.user.username;
+      const todoDb = await todoModel.findOne({ _id: todoId });
+      if (todoDb.username !== username) {
+        return res.status(403).send({
+          message: "Not authorized to edit the todo.",
+        });
+      }
+
+      const prevTodo = await todoModel.findOneAndUpdate(
+        { _id: todoId },
+        { task: updatedTask, priority: newPriority }
+      );
+      return res.status(200).send({
+        message: "Edit success",
+        data: prevTodo,
+      });
+    } catch (error) {
+      if (error.name === "ValidationError") res.status(400);
+      else res.status(500);
+      res.send({
         error,
       });
+    }
+  })
+  .post("/delete-todo", isAuth, async (req, res) => {
+    try {
+      const { todoId } = req.body;
+      const username = req.session.user.username;
+      const todoDb = await todoModel.findOne({ _id: todoId });
+      console.log(todoDb);
+      if (todoDb.username !== username) {
+        return res.status(403).send({
+          message: "Not authorized to delete this task.",
+        });
+      }
+
+      const prevTodo = await todoModel.findOneAndDelete({ _id: todoId });
+      return res.status(202).send({
+        message: "Delete success.",
+        data: prevTodo,
+      });
+    } catch (error) {
+      res.status(500).send(error);
     }
   });
 
